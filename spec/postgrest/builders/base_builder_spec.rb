98af3bf09d4cf81ec3af15293da4efcdb4044d54
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-05-03 23:09:40 -0300

    test: base builder

--- spec/postgrest/builders/base_builder_spec.rb
+++ spec/postgrest/builders/base_builder_spec.rb
@@ -1,2 +1,25 @@
 RSpec.describe Postgrest::Builders::BaseBuilder do
+  describe 'call/execute' do
+    let(:http_instance) { double(Postgrest::HTTP) }
+    before { allow(http_instance).to receive(:call) }
+
+    subject { described_class.new(http_instance) }
+
+    it { expect(subject).to respond_to(:call) }
+    it { expect(subject).to respond_to(:execute) }
+
+    context 'when trigger' do
+      it 'calls the #call method' do
+        subject.call
+
+        expect(http_instance).to have_received(:call)
+      end
+
+      it 'executes the #call method' do
+        subject.execute
+
+        expect(http_instance).to have_received(:call)
+      end
+    end
+  end
 end
